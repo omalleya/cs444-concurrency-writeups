74f7dc6c1778947f1cbbd72a2fe4a1e1c52028d0
omalleya <omalleya@oregonstate.edu>
2017-10-24 09:30:15 -0700

    threads take in their specific philosopher struct as an argument on creation

--- assign2/concurrency2.c
+++ assign2/concurrency2.c
@@ -4,6 +4,7 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <string.h>
 
 struct philosopher {
     int num;
@@ -11,7 +12,7 @@ struct philosopher {
     char* state;
 };
 
-//const char* phil_names[] = {"","","","",""};
+char* phil_names[] = {"Aristotle","Plato","Voltaire","blah","blah"};
 
 /*threads*/
 pthread_t phil_thread[5];
@@ -26,13 +27,13 @@ void think()
     sleep(wait);
 }
 
-void get_forks(int i)
+void get_forks(struct philosopher *phil)
 {
     //left fork lock
-    if(pthread_mutex_lock(&mutex[i]) != 0)
+    if(pthread_mutex_lock(&mutex[phil->num]) != 0)
         printf("failed");
     //right fork lock
-    if(pthread_mutex_lock(&mutex[(i+1)%5]) != 0)
+    if(pthread_mutex_lock(&mutex[(phil->num+1)%5]) != 0)
         printf ("failed");
 }
 
@@ -42,31 +43,31 @@ void eat()
     sleep(wait);
 }
 
-void put_forks(int i)
+void put_forks(struct philosopher *phil)
 {
     //left fork unlock
-    if(pthread_mutex_unlock(&mutex[i]) != 0)
+    if(pthread_mutex_unlock(&mutex[phil->num]) != 0)
         printf("failed");
     //right fork unlock
-    if(pthread_mutex_unlock(&mutex[(i+1)%5]) != 0)
+    if(pthread_mutex_unlock(&mutex[(phil->num+1)%5]) != 0)
         printf ("failed");
 }
 
 void *loop(void *i)
 {
-    int j = (intptr_t) i;
+    struct philosopher *j = (struct philosopher*) i;
     //free(i);
-    while(1)
-    {
-        printf("philosopher %d is thinking", j);
-        think();
-        get_forks(j);
-        printf("philosopher %d is eating", j);
-        eat();
-        put_forks(j);
-        return(NULL);
-    }
-
+    // while(1)
+    // {
+        printf("philosopher %s is thinking", j->name);
+        // think();
+        // get_forks(j);
+        // printf("philosopher %d is eating", j);
+        // eat();
+        // put_forks(j);
+    // }
+    return(NULL);
+        
 }
 
 void init()
@@ -94,25 +95,27 @@ int main()
     printf("test");
     /*create philosophers */
     struct philosopher *philosophers = malloc(sizeof(struct philosopher) * 5);
-    philosophers[0].name = "Aristotle";
 
-    printf(philosophers[0].name);
+    int i=0;
+    for(i=0; i<5; i++)
+    {
+        philosophers[i].num = i;
+        philosophers[i].name = phil_names[i];
+        philosophers[i].state = "thinking";
+
+    }
     
     /* initialize mutex and cond threads */
     init();
 
     /*create threads and join*/
-    int i=0;
     for(i=0; i<5; i++)
     {
-        int *j = malloc(sizeof(*j));
-        *j = i;
         pthread_create(&(phil_thread[i]),
             NULL,
             (void *) loop,
-            (void *)(intptr_t) i);
+            (void *) &philosophers[i]);
     }
-    i=0;
     for(i=0; i<5; i++)
     {
         pthread_join(phil_thread[i], NULL);
